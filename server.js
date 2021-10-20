dea0b4cb54636001fc279426dbdd37d87d72f072
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-10-21 00:23:58 +0300

    graphql server ready

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log()
+const express = require('express')
+const {ApolloService, gql} = require('apollo-server-express')
+
+const typeDefs = gql`
+     type Query {
+          hello:String;
+     }
+`
+
+const resolvers = {}
+
+async function startServer() {
+  const app = express()
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  })
+
+  await apolloServer.start()
+
+  app.use((req, res) => {
+    res.send('Hello from express apollo server')
+  })
+
+  apolloServer.applyMiddleware = {app: app}
+  app.listen(4000, () => console.log('listening on port 4000'))
+}
+startServer()
